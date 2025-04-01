00d60f7adfe6a625ee54a397c7e9562ec1728bd0
pranav-8660 <pranavcse2003@gmail.com>
2025-04-01 17:15:12 +0530

    empty springboot application

---
+++ test-springboot-application/src/main/java/com/pranavv51/microservices/test/test_springboot_application/TestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.microservices.test.test_springboot_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootApplication.class, args);
+	}
+
+}
---
+++ test-springboot-application/src/test/java/com/pranavv51/microservices/test/test_springboot_application/TestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.microservices.test.test_springboot_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
